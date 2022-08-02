3d55aee2349c3c10475e9a8beadbe65f3046ca93
lxw <2332425872@qq.com>
2022-08-02 10:27:20 +0800

    项目上传

---
+++ src/test/java/com/lvxw/springsecuritylearning/SpringSecurityLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvxw.springsecuritylearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
